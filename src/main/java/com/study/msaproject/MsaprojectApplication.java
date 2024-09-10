22006c4a9509d242ffd7939e7b5beac8819596d4
BanJung <higkdl813@gmail.com>
2024-09-10 15:03:22 +0900

    init project

---
+++ src/main/java/com/study/msaproject/MsaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.study.msaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaprojectApplication.class, args);
+    }
+
+}
